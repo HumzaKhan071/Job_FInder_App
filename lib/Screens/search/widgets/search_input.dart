3e9c7d72d2d0654f94eea64f790e7b838d1ad620
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2021-11-30 18:13:14 +0500

    update

--- lib/Screens/search/widgets/search_input.dart
+++ lib/Screens/search/widgets/search_input.dart
@@ -15,12 +15,30 @@ class SearchInput extends StatelessWidget {
                 filled: true,
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none),hintText: "Search",hintStyle: TextStyle(color: Colors.grey,fontSize: 18)),
+                    borderSide: BorderSide.none),
+                hintText: "Search",
+                hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                contentPadding: EdgeInsets.zero,
+                prefixIcon: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Image.asset(
+                    "assets/icons/search.png",
+                    width: 20,
+                  ),
+                )),
           )),
           SizedBox(
             width: 10,
           ),
-          Container(),
+          Container(
+            height: 50,
+            width: 50,
+            padding: EdgeInsets.all(13),
+            decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(20)),
+                child: Icon(),
+          ),
         ],
       ),
     );
