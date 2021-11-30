f39ecc796e3f0cfb9f251f42771d389e61731607
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2021-11-30 17:34:01 +0500

    updatte

--- lib/Screens/home/Widgets/search_card.dart
+++ lib/Screens/home/Widgets/search_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_finder_app/Screens/search/search.dart';
 
 class SearchCard extends StatelessWidget {
   const SearchCard({Key? key}) : super(key: key);
@@ -33,26 +34,32 @@ class SearchCard extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          Container(
-            padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
-            child: Row(
-              children: [
-                Image.asset(
-                  "assets/icons/search.png",
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "Search",
-                  style: TextStyle(color: Colors.grey, fontSize: 18),
-                )
-              ],
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => SearchPage()));
+            },
+            child: Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/icons/search.png",
+                    width: 20,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Search",
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  )
+                ],
+              ),
             ),
           )
         ],
